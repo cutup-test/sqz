9f70e7d12e755de1c156487592edf426cce9a5d0
Marcin Delektowski <marcin.delektowski@gmail.com>
2018-05-22 18:26:35 +0200

    Build banner is almost working

--- app/assets/scripts/modules/Slideshow.js
+++ app/assets/scripts/modules/Slideshow.js
@@ -2,51 +2,59 @@ class Slideshow {
   constructor() {
     this.banner = document.getElementsByClassName('banner_img')[0];
     this.pagination = document.getElementsByClassName('pagination');
-    this.clickEvent();
+    this.pauseButton = document.getElementsByClassName('pagination__pause')[0];
     this.slideChange();
   }
 
   paginationArray() {
-    let paginationAray = Array.from(this.pagination);
-    return paginationAray;
+    let paginationArray = Array.from(this.pagination);
+    return paginationArray;
   }
 
   removeRedFromPagination() {
     this.paginationArray().map((paginationElement) => paginationElement.classList.remove('pagination--red'));
   }
 
-  clickEvent() {
-    this.paginationArray().map((paginationElement) => paginationElement.addEventListener('click', this.changePaginationColor.bind(this)))
-  }
-
-  changePaginationColor(clickedPaginationElement) {
-    this.removeRedFromPagination();
-    clickedPaginationElement.target.classList.add('pagination--red');
-  }
-
   slideChange(clickedSlide = 1) {
     this.paginationArray()[clickedSlide - 1].classList.add('pagination--red');
     let that = this;
+    let slideInterval = setInterval(nextSlide, 2000);
     let slides = document.getElementsByClassName('banner__img')[0];
     let currentSlide = clickedSlide;
-    let nextSlide = () => {
+
+    this.paginationArray().map((paginationElement) => paginationElement.onclick = function (paginationElement) {
+      that.removeRedFromPagination();
+      paginationElement.target.classList.add('pagination--red');
+      stopInterval(this);
+    });
+    this.pauseButton.onclick = function () {
+      let buttonState = that.pauseButton.getAttribute('data-button-state');
+      if (buttonState === 'on') {
+        that.pauseButton.classList.toggle('pagination__pause--red');
+        clearInterval(slideInterval);
+        that.pauseButton.setAttribute('data-button-state', 'off');
+        that.slideChange(2);
+      }
+      that.pauseButton.setAttribute('data-button-state', 'on');
+      that.pauseButton.classList.toggle('pagination__pause--red');
+      clearInterval(slideInterval);
+    };
+
+    function nextSlide() {
       if (currentSlide === 6) currentSlide = 1;
       slides.src = `assets/images/slide${currentSlide}.jpg`;
-      this.removeRedFromPagination();
-      this.paginationArray()[currentSlide - 1].classList.add('pagination--red');
+      that.removeRedFromPagination();
+      that.paginationArray()[currentSlide - 1].classList.add('pagination--red');
       currentSlide++;
     }
-    this.paginationArray().map((paginationElement) => paginationElement.addEventListener('click', function () {
-      stopInterval(this);
 
-    }));
     function stopInterval(eventElement) {
       let slideNumber = eventElement.getAttribute("data-slide-number");
       clearInterval(slideInterval);
       slides.src = `assets/images/slide${slideNumber}.jpg`;
       that.slideChange(slideNumber);
     }
-    let slideInterval = setInterval(nextSlide, 2000);
   }
 }
+
 export default Slideshow;
